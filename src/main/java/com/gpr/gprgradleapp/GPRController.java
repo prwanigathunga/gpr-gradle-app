1b59142af1ba7762ace63204ac7ab9ba58697e38
ranga <prwanigathunga@gmail.com>
2024-02-25 17:42:09 +0530

    all commit

--- src/main/java/com/gpr/gprgradleapp/GPRController.java
+++ src/main/java/com/gpr/gprgradleapp/GPRController.java
@@ -1,2 +1,8 @@
-package com.gpr.gprgradleapp;public class GPRController {
+package com.gpr.gprgradleapp;
+
+public class GPRController {
+
+    public void gprFunction(){
+        System.out.println("This is a test...................");
+    }
 }
---
+++ src/main/java/com/gpr/gprgradleapp/GprGradleAppApplication.java
@@ -0,0 +1,13 @@
+package com.gpr.gprgradleapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GprGradleAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GprGradleAppApplication.class, args);
+    }
+
+}
